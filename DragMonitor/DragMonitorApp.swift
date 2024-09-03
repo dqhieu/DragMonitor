ef202d84ede3e56be98cec7b2972dca59209c728
dqhieu <dqhieu13@gmail.com>
2024-09-03 22:04:51 +0800

    first commit

---
+++ DragMonitor/DragMonitorApp.swift
@@ -0,0 +1,56 @@
+//
+//  DragMonitorApp.swift
+//  DragMonitor
+//
+//  Created by Dinh Quang Hieu on 3/9/24.
+//
+
+import SwiftUI
+import AppKit
+
+@main
+struct DragMonitorApp: App {
+
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  var dragMonitor: Any?
+  var lastPasteboardChangeCount: Int = 0
+
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    dragMonitor = NSEvent.addGlobalMonitorForEvents(matching: .leftMouseDragged) { _ in
+      self.checkForDraggedItems()
+    }
+  }
+
+  func checkForDraggedItems() {
+    let dragPasteboard = NSPasteboard(name: .drag)
+    let currentChangeCount = dragPasteboard.changeCount
+    guard lastPasteboardChangeCount != currentChangeCount else {
+      return
+    }
+    lastPasteboardChangeCount = currentChangeCount
+    let dragTypes = [NSPasteboard.PasteboardType.URL]
+    if dragPasteboard.availableType(from: dragTypes) != nil {
+      print("💛 dragging")
+      if let items = dragPasteboard.pasteboardItems {
+        items.forEach { item in
+          if let urls = dragPasteboard.readObjects(forClasses: [NSURL.self]) as? [URL] {
+            print("💛", urls.map(\.path))
+          }
+        }
+      }
+
+
+    }
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# DragMonitor
+
+Get notified when user starts dragging a file outside of the app
+
+Demo: https://twitter.com/hieudinh_/status/1830969866595217579
